7042c9849c428bdd58568079d89aa2043f382a9c
Mahdis8160 <mahdis8160@gmail.com>
2023-01-08 13:38:33 -0800

    challenge 4

---
+++ 01-Activities/02-Stu_Window-Object/Unsolved/script.js
@@ -0,0 +1,12 @@
+// The window object that represents the browser window
+console.log(window);
+
+// The HTML document loaded in the window
+console.log(window.document);
+
+// The <html> element in the document
+console.log(document.documentElement);
+
+// The <head> element in the document
+console.log(document.head);
+
